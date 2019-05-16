f42c83953c7b19a0ef751f246b8bda79dab828cc
Paul K <pkfis@outlook.com>
2019-05-16 23:55:31 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,35 @@
+class Genre
+
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist
+    @songs << song unless @songs.include?(song)
+  end
+
+end
