799b26fcbeafd81208c29248aebc967712f244d0
Paul K <pkfis@outlook.com>
2019-05-17 00:37:42 +0000

    Automatically backed up by Learn

--- lib/concerns/concerns.rb
+++ lib/concerns/concerns.rb
@@ -1,13 +1,11 @@
-module Concerns
+module Concerns::Findable
   # Module code here
-  module Findable
-    def find_by_name(name)
-      self.all.detect{|k| k.name == name }
-    end
+  def find_by_name(name)
+    self.all.detect{|k| k.name == name }
+  end
 
-    def find_or_create_by_name(name)
-      self.find_by_name(name) || create(name)
-    end
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || create(name)
   end
 
 end
