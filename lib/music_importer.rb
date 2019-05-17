78ef5935cbcd5f6e152539cd446f1b77b892e7df
Paul K <pkfis@outlook.com>
2019-05-17 00:54:39 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -10,7 +10,7 @@ class MusicImporter
     @files ||= Dir.glob("#{path}/*.mp3").collect {|file| file.gsub("#{path}/", "") }
   end
 
-  def self.import
+  def import
     files.each{|file| Song.create_from_filename(file)}
   end
 
