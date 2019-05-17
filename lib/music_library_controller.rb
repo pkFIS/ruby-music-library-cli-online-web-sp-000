78b57e1f0ee9cf53d5c5b581916fdcd1076c7de8
Paul K <pkfis@outlook.com>
2019-05-17 00:59:10 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -14,7 +14,22 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
 
+    input = gets.strip
 
+    case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+    end
   end
 
 end
