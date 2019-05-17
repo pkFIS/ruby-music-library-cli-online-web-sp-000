a69a03a86615468679ddb7476a7b645daaeaea89
Paul K <pkfis@outlook.com>
2019-05-17 00:01:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,5 +26,13 @@ class Song
     song.save
   end
 
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
 
 end
